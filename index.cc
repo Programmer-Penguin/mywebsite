64a9f37e77582d23dd6140902bdb1066a241eb1b
Programmer Penguin <129675740+master-gamer360@users.noreply.github.com>
2024-07-01 23:59:35 +0500

    Create index.cc

---
+++ index.cc
@@ -0,0 +1,28 @@
+a
+{
+    color: red;
+    text-decoration: none;
+}
+a:hover
+{
+    text-decoration: underline;
+    color: green;
+}
+
+button
+{
+    background-color: black;
+    color: white;
+}
+
+body
+{
+    text-align: center;
+    background-color: black;
+}
+
+input
+{
+    background-color: black;
+    color: white;
+}
