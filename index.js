f8833673947797ec9d003e3b6ef6278a887c40cd
Programmer Penguin <abubakarakamastergamer360@gmail.com>
2024-07-04 00:11:58 +0500

    lot of vissual changes

--- index.js
+++ index.js
@@ -8,7 +8,27 @@ localStorage.setItem("page_view", visitCount);
 document.getElementById("counter-value").innerHTML = visitCount;
 
 // Reset the counter
-function resetCounter() {
+function resetCounter()
+{
     localStorage.removeItem("page_view");
     document.getElementById("counter-value").innerHTML = 1;
+}
+const sleep = async (milliseconds) => {
+    await new Promise(resolve => {
+        return setTimeout(resolve, milliseconds)
+    });
+};
+
+// FizzBuzz function
+function FizzBuzz()
+{
+    for (let i = 0; i <= 100; i++)
+    {
+        let output = "";
+        if (i % 3 == 0) {output += "Fizz";}
+        if (i % 5 == 0) {output += "Buzz";}
+        else {output = i}
+        document.getElementById("fizzbuzz_text").innerHTML = output
+        sleep(1000)
+    }
 }
