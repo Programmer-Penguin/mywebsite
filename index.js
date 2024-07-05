c65a93e740450a5dbd4fd5e7156f7b3db57567d1
Programmer Penguin <abubakarakamastergamer360@gmail.com>
2024-07-06 00:08:23 +0500

    added a fizzbuzz reset button

--- index.js
+++ index.js
@@ -13,31 +13,45 @@ function resetCounter()
     localStorage.removeItem("page_view");
     document.getElementById("counter-value").innerHTML = 1;
 }
-function sleep(ms) {
+function sleep(ms) 
+{
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
 // FizzBuzz function
-async function FizzBuzz() {
-    for (let i = 0; i <= 100; i++) {
+async function FizzBuzz() 
+{
+    document.getElementById("fizzbuzz_text").innerHTML = "";
+    await sleep(1000);
+    for (let i = 0; i <= 100; i++)
+    {
         let output = "";
-        if (i % 3 == 0) {
+        if (i % 3 == 0) 
+        {
             output += "Fizz";
         }
-        if (i % 5 == 0) {
+        if (i % 5 == 0) 
+        {
             output += "Buzz";
-        } else if (output === "") {
+        } else if (output === "") 
+        {
             output = i;
         }
         // Append the output to an existing element (e.g., a <div> with id "fizzbuzz_text")
-        document.getElementById("fizzbuzz_text").innerHTML += output + "<br><br>"; // Add a line break
-        await sleep(70);
+        document.getElementById("fizzbuzz_text").innerHTML += output + "<br>"; // Add a line break
+        await sleep(5);
         window.scrollTo(0, document.body.scrollHeight + 10);
     }
 
 }
-document.querySelector('form').addEventListener('submit', function(event) {
+function FB_reset()
+{
+    document.getElementById("fizzbuzz_text").innerHTML = "<br>";
+}
+
+document.querySelector('form').addEventListener('submit', function(event) 
+{
     alert('hello, ' + document.querySelector('#name').value);
     event.preventDefault();
 });
