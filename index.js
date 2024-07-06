437810f5e69165a1f85b81e636ca82b99d42025a
Programmer Penguin <abubakarakamastergamer360@gmail.com>
2024-07-06 17:54:07 +0500

    added a light mode theme choice from the theme selector

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var elements = [];
+
+elements.push(document.querySelector('body'));
+elements.push(document.querySelector('h1'));
+elements.push(document.querySelectorAll("button"));
+
+
+var radio_buttons = [];
+radio_buttons.push(document.getElementById("light_radio"));
+radio_buttons.push(document.getElementById("dark_radio"));
+
+var radio_result = document.getElementById("radio_result");
+
 var visitCount = localStorage.getItem("page_view") || 1;
 
 // Increment the count on each visit
@@ -13,21 +26,22 @@ function resetCounter()
     localStorage.removeItem("page_view");
     document.getElementById("counter-value").innerHTML = 1;
 }
-function sleep(ms) 
+
+function sleep(ms)
 {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
 // FizzBuzz function
-async function FizzBuzz() 
+async function FizzBuzz()
 {
     document.getElementById("fizzbuzz_text").innerHTML = "";
     await sleep(1000);
     for (let i = 0; i <= 100; i++)
     {
         let output = "";
-        if (i % 3 == 0) 
+        if (i % 3 == 0)
         {
             output += "Fizz";
         }
@@ -39,7 +53,7 @@ async function FizzBuzz()
             output = i;
         }
         // Append the output to an existing element (e.g., a <div> with id "fizzbuzz_text")
-        document.getElementById("fizzbuzz_text").innerHTML += output + "<br>"; // Add a line break
+        document.getElementById('fizzbuzz_text').innerHTML += output + '<br>'; // Add a line break
         await sleep(5);
         window.scrollTo(0, document.body.scrollHeight + 10);
     }
@@ -50,8 +64,39 @@ function FB_reset()
     document.getElementById("fizzbuzz_text").innerHTML = "<br>";
 }
 
-document.querySelector('form').addEventListener('submit', function(event) 
+function change_theme()
 {
-    alert('hello, ' + document.querySelector('#name').value);
-    event.preventDefault();
-});
+    // if light mode:
+    if (radio_buttons[0].checked)
+    {
+        elements[0].style.backgroundColor = 'white';
+        elements[1].style.color = 'black';
+
+        elements[2].forEach(function(button)
+        {
+            button.style.color = 'black';
+            button.style.backgroundColor = 'white';
+        });
+        document.getElementsByClassName("theme_submit_button")[0].style.color = 'grey';
+        document.getElementById("welcome_sign").innerHTML = "";
+        document.getElementsByClassName("penguin_pfp")[0].style.border = "5px solid black";
+    }
+    // elif dark mode:
+    else if (radio_buttons[1].checked)
+    {
+        elements[0].style.backgroundColor = 'black';
+        elements[1].style.color = 'blue';
+
+        elements[2].forEach(function(button) 
+        {
+            button.style.color = 'blue';
+            button.style.backgroundColor = 'black';
+        });
+        document.querySelector('button').style.color = 'grey';
+    }
+    // else idk:
+    else
+    {
+        console.log("idk man");
+    }
+}
